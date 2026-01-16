f073034d3520f64b142c1264c2980967a7fc128d
AniketPastapure <pastapure.aniket@gmail.com>
2026-01-16 18:33:06 +0530

    first commit

---
+++ src/main/java/com/example/jenkin/jenkinSpring/JenkinSpringApplication.java
@@ -0,0 +1,21 @@
+package com.example.jenkin.jenkinSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JenkinSpringApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(JenkinSpringApplication.class);
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkin/jenkinSpring/JenkinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin.jenkinSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
