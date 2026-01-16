c78e95bb72d291593fc8d362ef8805c3a2aeaff7
AniketPastapure <pastapure.aniket@gmail.com>
2026-01-16 19:12:51 +0530

    dele

--- src/main/java/com/example/jenkin/jenkinSpring/controller/jenkinController.java
+++ src/main/java/com/example/jenkin/jenkinSpring/controller/jenkinController.java
@@ -1,14 +1,14 @@
 package com.example.jenkin.jenkinSpring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+
 public class jenkinController {
   
-	@GetMapping("/home/name")
+	@GetMapping("/name")
 	public String getName() {
 		return "Hello World";
 	}
